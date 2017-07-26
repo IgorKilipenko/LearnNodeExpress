4b4bb05ecdb9a3294031bd140eb1871a7b0fe8dd
IgorKilipenko <igorkili@mail.ru>
2017-07-26 13:30:14 +0700

    11

---
+++ HttpServer/hellowWorld.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 3000;
+const hostname = '127.0.0.1';
+
+http.createServer((req, res) =>{
+    serverStaticFile(res, "", 'text/html');
+}).listen(port, hostname, () => {
+    console.log(`Сервер запущен http://${hostname}:${port}/. Нажмите Ctr+C для остановки`);
+});
+
+function serverStaticFile(res, path, contentType, responseCode) {
+    if (!responseCode)
+        responseCode = 200;
+    fs.readFile(__dirname + path, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'ContentType': 'text/plain' });
+            res.end('500 - Internal error');
+        } else {
+            res.writeHead(responseCode, { 'ContentType': contentType });
+            res.end(data);
+        }
+    })
+}
